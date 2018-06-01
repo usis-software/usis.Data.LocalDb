c07bcad24334c27d6b0da22016af5399d843cc0b
Udo Schäfer <17689704+Udo-usis@users.noreply.github.com>
2018-06-01 07:51:29 +0000

    documentation

--- LocalDbException.cs
+++ LocalDbException.cs
@@ -17,18 +17,52 @@ namespace usis.Data.LocalDb
     //  LocalDbException class
     //  ----------------------
 
+    /// <summary>
+    /// Throws an exception for a SQL Server Express LocalDB error.
+    /// </summary>
+    /// <seealso cref="Win32Exception" />
+
     [Serializable]
     public class LocalDbException : Win32Exception
     {
+        #region construction
+
+        //  ------------
+        //  construction
+        //  ------------
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalDbException"/> class.
+        /// </summary>
+
         public LocalDbException() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalDbException"/> class.
+        /// </summary>
+        /// <param name="message">A detailed description of the error.</param>
 
         public LocalDbException(string message) : base(message) { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalDbException"/> class.
+        /// </summary>
+        /// <param name="message">A detailed description of the error.</param>
+        /// <param name="innerException">A reference to the inner exception that is the cause of this exception.</param>
+
         public LocalDbException(string message, Exception innerException) : base(message, innerException) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalDbException"/> class.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo" /> associated with this exception.</param>
+        /// <param name="context">A <see cref="StreamingContext" /> that represents the context of this exception.</param>
 
         protected LocalDbException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 
         internal LocalDbException(uint hr, string message) : base((int)hr, message) { }
+
+        #endregion construction
     }
 }
 
---
+++ StopInstanceOptions.cs
@@ -0,0 +1,45 @@
+﻿//
+//  @(#) StopInstanceOptions.cs
+//
+//  Project:    usis.Data.LocalDb
+//  System:     Microsoft Visual Studio 2017
+//  Author:     Udo Schäfer
+//
+//  Copyright (c) 2018 usis GmbH. All rights reserved.
+
+using System;
+
+namespace usis.Data.LocalDb
+{
+    //  -------------------------------
+    //  StopInstanceOptions enumeration
+    //  -------------------------------
+
+    /// <summary>
+    /// Describes to options that spectify the way to stop an instance.
+    /// </summary>
+
+    [Flags]
+    public enum StopInstanceOptions
+    {
+        /// <summary>
+        /// Shut down by using the SHUTDOWN Transact-SQL command. 
+        /// </summary>
+
+        None = 0x0,
+
+        /// <summary>
+        /// Shut down immediately using the kill process operating system command.
+        /// </summary>
+
+        ShutdownKillProcess = 0x1,
+
+        /// <summary>
+        /// Shut down using the WITH NOWAIT option Transact-SQL command.
+        /// </summary>
+
+        ShutdownWithNoWait = 0x2
+    }
+}
+
+// eof "StopInstanceOptions.cs"
