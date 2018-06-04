e76ce4daea5953ba61626c00206bd40a5af578b0
Udo Schäfer <17689704+Udo-usis@users.noreply.github.com>
2018-06-04 13:13:04 +0000

    AssemblyInfo.cs; usis.Data.LocalDb.nuspec

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -16,32 +16,20 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("usis.Data.SqlLocalDb")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("usis.Data.SqlLocalDb")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("usis LocalDB Class Library")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#endif
+[assembly: AssemblyCompany("usis GmbH")]
+[assembly: AssemblyProduct("usis LocalDB Class Library")]
+[assembly: AssemblyCopyright("Copyright © 2018 usis GmbH")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+// The assembly is not visible to COM components.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("73a1ae65-519e-42ed-b64f-1af244949778")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
