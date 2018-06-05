55c9b14922fa01ed95b97f6c6da7ef6de55cc6d1
usis <usis@3a870216-a665-8d43-957e-54433041a7f2>
2018-06-05 15:58:47 +0000

    ...

--- Windows/Data/usis.Data.LocalDb/Properties/AssemblyInfo.cs
+++ Windows/Data/usis.Data.LocalDb/Properties/AssemblyInfo.cs
@@ -12,9 +12,6 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("usis.Data.SqlLocalDb")]
 [assembly: AssemblyDescription("usis LocalDB Class Library")]
 #if DEBUG
